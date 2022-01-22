2fad3c948724b264ac119fd4f305a72d5e22cba4
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-01-23 01:33:33 +0300

    Initial commit

---
+++ app/src/main/java/com/metehanbolat/processdeath/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.metehanbolat.processdeath
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.metehanbolat.processdeath.ui.theme.ProcessDeathTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ProcessDeathTheme {
+
+                Surface(color = MaterialTheme.colors.background) {
+                    
+                }
+            }
+        }
+    }
+}
