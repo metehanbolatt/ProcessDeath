9d2f93e3d55dd854dab5edd77e2ce6692b64c7f9
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-01-23 01:47:37 +0300

    We can solve this problem by using "savedStateHandle"

--- app/src/main/java/com/metehanbolat/processdeath/MainViewModel.kt
+++ app/src/main/java/com/metehanbolat/processdeath/MainViewModel.kt
@@ -3,14 +3,16 @@ package com.metehanbolat.processdeath
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 
-class MainViewModel : ViewModel() {
+class MainViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel() {
 
-    var counter by mutableStateOf(0)
+    var counter by mutableStateOf(savedStateHandle.get("counter") ?: 0)
         private set
 
     fun count() {
         counter++
+        savedStateHandle.set("counter", counter)
     }
 }
